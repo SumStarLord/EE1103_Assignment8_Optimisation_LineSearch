e31b2232ef60fa90fcc67208010f0e717af135ac
= <=>
2023-02-02 15:52:59 +0530

    parabolic interpolation method added

---
+++ linesearch.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double f(double x){
+    return 2*sin(x) - (x*x)/10;
+}
+
+//returns the x value for maxima.
+double parabolic_interpolation(double (*f)(double x), double x_0, double x_1, double x_2, int num_iter){
+    double y_0, y_1, y_2, x_3, y_3;
+    for(int i = 0; i<num_iter; i++){
+        y_0 = f(x_0);
+        y_1 = f(x_1);
+        y_2 = f(x_2);
+        
+        x_3 = ((y_0 * (x_1*x_1 - x_2*x_2)) + (y_1 * (x_2*x_2 - x_0*x_0)) + (y_2 * (x_0*x_0 - x_1*x_1))) / ((2*y_0*(x_1 - x_2)) + (2*y_1*(x_2 - x_0)) + (2*y_2*(x_0 - x_1)));
+        y_3 = f(x_3);
+
+        //for debugging
+        printf("%d | %lf | %lf | %lf | %lf | %lf | %lf | %lf | %lf\n", i+1, x_0, y_0, x_1, y_1, x_2, y_2, x_3, y_3);
+
+        //updating for next iter
+        if(y_1 > y_3){
+            if(x_1 < x_3){
+                x_2 = x_3;
+            } else {
+                x_0 = x_3;
+            }
+        } else {
+            if(x_1 < x_3){
+                x_0 = x_1;
+                x_1 = x_3;
+            } else {
+                x_2 = x_1;
+                x_1 = x_3;
+            }
+        }
+    }
+    return x_3;
+}
+
+void main(){
+    double result = parabolic_interpolation(&f, 0,1,4,5);
+    printf("x_max = %lf\ny_max = %lf\n", result, f(result));
+}
