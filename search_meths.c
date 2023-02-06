55eabbc90cae2b6183359680537cd3467996d712
Jhumid <ee22b005@smail.iitm.ac.in>
2023-02-07 02:02:05 +0530

    Added golden and two thirds

---
+++ search_meths.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+double golden(double a, double b, double e, double (*func)(double x))
+{
+    double xl, xu,x1,x2,xopt;
+    xl = a;
+    xu = b;
+    double R = (sqrt(5) - 1)/2;
+    double d = R*(xu - xl);
+    x1 = xl + d;
+    x2 = xu - d;
+    bool exite = false;
+    
+    double error;
+    while(exite == false)
+    {
+        if (func(x1)>func(x2))
+        {
+           
+            xopt = x1;
+            error = (1-R)*fabs((xu-xl)/xopt);
+            xl = x2;
+            x2 = x1;
+            d = R*(xu - xl);
+            x1 = xl + d;
+            
+           
+        }
+        else if (func(x1)<func(x2))
+        {
+            xopt = x2;
+            error = (1-R)*fabs((xu-xl)/xopt);
+            xu = x1;
+            x1 = x2;
+            d = R*(xu - xl);
+            x2 = xu - d;
+            
+           
+        }
+        
+           if(error<e)
+           {
+             exite = true;
+           }
+           
+    }
+    return xopt;
+    
+}
+double twobythree(double a, double b, double e, double (*func)(double x))
+{
+    double xl, xu,x1,x2,xopt;
+    xl = a;
+    xu = b;
+    
+    double R = 2.0/3.0;
+    double d = R*(xu - xl);
+    
+    x1 = xl + d;
+    
+    x2 = xu - d;
+   
+    bool exite = false;
+    
+    double error;
+    while(exite == false)
+    {
+        if (func(x1)>func(x2))
+        {
+           
+            xopt = x1;
+            error = (1-R)*fabs((xu-xl)/xopt);
+            xl = x2;
+           
+            d = R*(xu - xl);
+            x1 = xl + d;
+            x2 = xu - d;
+           
+        }
+        else
+        {
+            
+            xopt = x2;
+            
+            error = (1-R)*fabs((xu-xl)/xopt);
+            xu = x1;
+            
+            d = R*(xu - xl);
+            x1 = xl + d;
+            x2 = xu - d;
+            
+           
+        }
+        
+           if(error<e)
+           {
+             exite = true;
+           }
+           
+    }
+    return xopt;
+    
+}
+double f (double x)
+{
+    return (-x*x + 5*x -6);
+}
+
+void main()
+{
+    double c = twobythree(-4.0,5.0,0.001,f);
+    printf("%f",c);
+}
